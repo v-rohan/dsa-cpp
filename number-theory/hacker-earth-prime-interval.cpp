77212619c4ae0c0a6da7cf189e076369b891d103
rohanrao <rohanrao@pop-os.localdomain>
2021-04-17 13:57:52 +0530

    added hacker-earth primality question

--- number-theory/hacker-earth-prime-interval.cpp
+++ number-theory/hacker-earth-prime-interval.cpp
@@ -5,8 +5,29 @@ Number theory - Primality
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 #define mod 1000000007
 #define lli long long int
 #define li long int
 #define vi vector<int>
+
+bool sieve[900001];
+vi primes;
+bool isPrime(int a)
+{
+    for (int i = 2; i * i <= a; i++)
+    {
+        if (a % i == 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    int l, r;
+    cin >> l >> r;
+    for (int i = l>2?l:2; i <= r; i++)
+        if (isPrime(i))
+            cout << i << " ";
+}
